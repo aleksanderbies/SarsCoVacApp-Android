7791aa0658b1e5371a7686e0b1acc857d319c7f4
Aleksander Bies <aleksanderbies@gmail.com>
2021-05-20 11:25:11 +0200

    search vaccination date by lands

---
+++ app/src/main/java/com/abies/sarscovacc/ChooseRegionActivity.kt
@@ -0,0 +1,11 @@
+package com.abies.sarscovacc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseRegionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_region)
+    }
+}
